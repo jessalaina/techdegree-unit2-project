3d114c3309edfadce42611111635bc7bd151336e
jessalaina <104215115+jessalaina@users.noreply.github.com>
2022-05-03 21:34:45 -0400

    Add comments
    Explanation of code through comments

--- js/script.js
+++ js/script.js
@@ -14,13 +14,17 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+// number of students per page
 const pageItems = 9;
 
+//function to show student data per page
 function showPage(list, page) {
     let startIndex = (page * pageItems) - pageItems;
     let endIndex = page * pageItems;
+   // add elements of student data to ul
     let studentList = document.querySelector('.student-list');
     studentList.innerHTML = '';
+   // loop through list of data to get needed student details
     for (let i = 0; i < list.length; i++) {
         if (i >= startIndex && i < endIndex) {
             let studentItem = `<li class="student-item cf"><div class="student-details"><img class="avatar" src="${list[i]['picture'].thumbnail}" alt="Profile Picture"><h3>${list[i]['name'].first} ${list[i]['name'].last}</h3><span class="email">${list[i].email}</span></div><div class="joined-details"><span class="date">${list[i]['registered'].date}</span></div></li>`;
@@ -35,11 +39,13 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+//function to add pagination to bottom of page
 function addPagination(list) {
+   // needed number of pages from total data vs 9 student profile per page
     let numOfPages = Math.ceil(list.length / 9);
     let linkList = document.querySelector('.link-list');
     linkList.innerHTML = '';
-
+   // loop through pages to create pagination buttons & add under student data
     for (let i = 1; i <= numOfPages; i++) {
         let button = `<li><button type="button">${i}</button></li>`
         linkList.insertAdjacentHTML('beforeend', button);
@@ -47,7 +53,7 @@ function addPagination(list) {
 
      let firstButton = document.querySelector('button');
      firstButton.className = 'active';
-     
+     // add event to list of items so that each page will show next 9 students data by changing active class per button
      linkList.addEventListener('click', (e) => {
         if (e.target.tagName === 'BUTTON') {
             document.querySelector('.active').className ='';
